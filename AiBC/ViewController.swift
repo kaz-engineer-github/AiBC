262e2efa18de80d63404dbb1c2e4f4f8f9553dc6
Kazufumi Yoshimoto <kazufumi2468australia@gmail.com>
2020-09-08 22:03:45 +0900

    Initial Commit

---
+++ AiBC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AiBC
+//
+//  Created by 吉本和史 on 2020/09/08.
+//  Copyright © 2020 吉本和史. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
