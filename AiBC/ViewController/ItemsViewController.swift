30a85f0fae0521cb373f896c060e7421ce398649
Kazufumi Yoshimoto <kazufumi2468australia@gmail.com>
2020-09-11 21:48:15 +0900

    set table

--- AiBC/ViewController/ItemsViewController.swift
+++ AiBC/ViewController/ItemsViewController.swift
@@ -13,6 +13,7 @@ class ItemsViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     private var dataSource: [ItemsViewControllerCellType] = []
     private let itemsModel = ItemsModel()
+    var articleUrl: URL?
   
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,13 +45,47 @@ extension ItemsViewController: UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: ItemsViewControllerCell.reuseIdentifier, for: indexPath) as! ItemsViewControllerCell
-        return cell
+        let cellType = dataSource[indexPath.row]
+        
+        switch cellType {
+        case .item(let item):
+            let cell = tableView.dequeueReusableCell(withIdentifier: ItemsViewControllerCell.reuseIdentifier, for: indexPath) as! ItemsViewControllerCell
+            
+            cell.configureCell(
+                profileImageURL: item.user.profileImageURL,
+                title: item.title,
+                body: item.body,
+                tags: item.tags.reduce("") { $0 + "#\($1.name) " },
+                bookmarkCount: item.likesCount,
+                commentsCount: item.commentsCount,
+                url: item.url)
+            return cell
+        case .indicator:
+            let cell = tableView.dequeueReusableCell(withIdentifier: ItemsViewControllerIndicatorCell.reuseIdentifier, for: indexPath) as! ItemsViewControllerIndicatorCell
+            cell.configureCell()
+            return cell
+        }
     }
 }
 
 extension ItemsViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cellType = dataSource[indexPath.row]
         
+            switch cellType {
+            case .item(let item):
+                guard let url = URL(string: item.url) else { return }
+                articleUrl = url
+                self.performSegue(withIdentifier: "WebViewController", sender: nil)
+            default:
+                break
+            }
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "WebViewController" {
+            let webVC: WebViewController = (segue.destination as? WebViewController)!
+            webVC.url = articleUrl
+        }
     }
 }
+
