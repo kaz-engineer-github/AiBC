b5e9a2e27d2237aa11dd469b441abb7203e3248e
Kazufumi Yoshimoto <kazufumi2468australia@gmail.com>
2020-09-21 10:31:29 +0900

    delete unnecessary comments

--- AiBC/ViewController/WebViewController.swift
+++ AiBC/ViewController/WebViewController.swift
@@ -16,21 +16,10 @@ class WebViewController: UIViewController {
   
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(webView)
       
         let request = URLRequest(url: url!)
         webView.load(request)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
