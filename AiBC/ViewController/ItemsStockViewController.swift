607266cba66f8ab5348378a5f9c353df8923fb13
Kazufumi Yoshimoto <kazufumi2468australia@gmail.com>
2020-09-22 18:41:53 +0900

    create ItemsStockViewController.swift and chancge folder structure

---
+++ AiBC/ViewController/ItemsStockViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ItemsStockViewController.swift
+//  AiBC
+//
+//  Created by 吉本和史 on 2020/09/22.
+//  Copyright © 2020 吉本和史. All rights reserved.
+//
+
+import UIKit
+
+class ItemsStockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
