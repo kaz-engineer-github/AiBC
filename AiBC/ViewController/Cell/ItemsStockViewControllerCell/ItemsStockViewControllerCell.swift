69619715f78a0a496eb300e61646897ef3facaf8
Kazufumi Yoshimoto <kazufumi2468australia@gmail.com>
2020-09-28 12:00:20 +0900

    change folder structure and add new file named ItemsStockViewControllerCell

---
+++ AiBC/ViewController/Cell/ItemsStockViewControllerCell/ItemsStockViewControllerCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemsStockViewControllerCell.swift
+//  AiBC
+//
+//  Created by 吉本和史 on 2020/09/28.
+//  Copyright © 2020 吉本和史. All rights reserved.
+//
+
+import UIKit
+
+class ItemsStockViewControllerCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
