9cf8536cc12b8988a3c2178d0b41a9cc4e8c08f6
Kazufumi Yoshimoto <kazufumi2468australia@gmail.com>
2020-09-29 14:17:59 +0900

    set nib name

--- AiBC/ViewController/Cell/ItemsStockViewControllerCell/ItemsStockViewControllerCell.swift
+++ AiBC/ViewController/Cell/ItemsStockViewControllerCell/ItemsStockViewControllerCell.swift
@@ -10,15 +10,15 @@ import UIKit
 
 class ItemsStockViewControllerCell: UITableViewCell {
 
+    static let reuseIdentifier: String = "ItemsStockViewControllerCell"
+    static let nib: UINib = UINib(nibName: "ItemsStockViewControllerCell", bundle: nil)
+  
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
 }
