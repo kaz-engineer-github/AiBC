088f07279fb25c0500a8e846d7ff950bb3d90088
Kazufumi Yoshimoto <kazufumi2468australia@gmail.com>
2020-10-11 15:13:27 +0900

    change layout

--- AiBC/ViewController/Cell/ItemsStockViewControllerCell/ItemsStockViewControllerCell.swift
+++ AiBC/ViewController/Cell/ItemsStockViewControllerCell/ItemsStockViewControllerCell.swift
@@ -14,7 +14,6 @@ class ItemsStockViewControllerCell: UITableViewCell {
     @IBOutlet weak var profileIconImage: UIImageView!
     @IBOutlet weak var bodyLabel: UILabel!
     @IBOutlet weak var tagsLabel: UILabel!
-    @IBOutlet weak var urlLabel: UILabel!
     private let itemsStockModel = BookmarkModel()
     static let reuseIdentifier: String = "ItemsStockViewControllerCell"
     static let nib: UINib = UINib(nibName: "ItemsStockViewControllerCell", bundle: nil)
@@ -26,26 +25,5 @@ class ItemsStockViewControllerCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-    }
-  
-
-  
-    @IBAction func trashArticle(_ sender: Any) {
-        articleData.title = titleLabel.text ?? ""
-        articleData.profileImageURL = profileIconImage.image?.toString()
-
-        print("profileImageURL===>\(articleData.profileImageURL)")
-      
-        articleData.body = bodyLabel.text ?? ""
-        articleData.tags = tagsLabel.text ?? ""
-        articleData.url = urlLabel.text ?? ""
-        itemsStockModel.trashBookmarkArticles(title: articleData.title, profileImageURL: articleData.profileImageURL ?? "", body: articleData.body, tags: articleData.tags, url: articleData.url)
-    }
-}
-
-extension UIImage {
-    func toString() -> String? {
-        let data: Data? = UIImage.init().pngData()
-        return data?.base64EncodedString(options: .endLineWithLineFeed)
     }
 }
