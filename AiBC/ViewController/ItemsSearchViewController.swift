9186c4924d0574f3bc006e6f30b44a6e9d928fb0
Kazufumi Yoshimoto <kazufumi2468australia@gmail.com>
2020-10-14 11:04:10 +0900

    modify send article data

--- AiBC/ViewController/ItemsSearchViewController.swift
+++ AiBC/ViewController/ItemsSearchViewController.swift
@@ -108,6 +108,7 @@ extension ItemsSearchViewController: UITableViewDelegate {
             case .item(let item):
                 articleData.title = item.title
                 articleData.profileImageURL = item.user.profileImageURL
+                articleData.body = item.body
                 articleData.tags = item.tags.reduce("") { $0 + "#\($1.name) " }
                 articleData.url = item.url
               
