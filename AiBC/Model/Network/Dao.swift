5871ee6c53d7a1c364c26e40f629666da63b62ae
Kazufumi Yoshimoto <kazufumi2468australia@gmail.com>
2020-11-21 14:39:54 +0900

    add getDocument code

--- AiBC/Model/Network/Dao.swift
+++ AiBC/Model/Network/Dao.swift
@@ -16,29 +16,38 @@ final class Dao {
     // MARK: - Access DB to preserve articles data
     //DBに直接アクセスして、記事データを更新(初期設定)する
     func updateData(title: [String], profileImageURL: [String], body: [String], tags: [String], url: [String]) {
-        let bookmarkRef = db.collection(Constants.FStore.collectionName).document(Constants.FStore.documentName)
-        //データの更新
-        bookmarkRef.updateData([
-            //配列データに追加
-            Constants.FStore.titleField: FieldValue.arrayUnion(title),
-            Constants.FStore.profField: FieldValue.arrayUnion(profileImageURL),
-            Constants.FStore.bodyField: FieldValue.arrayUnion(body),
-            Constants.FStore.tagsField: FieldValue.arrayUnion(tags),
-            Constants.FStore.urlField: FieldValue.arrayUnion(url)
-        ]) { (error) in
-            if error != nil {
+        let ref = db.collection(Constants.FStore.collectionName).document(Constants.FStore.documentName)
+        //データがあるか読み取り
+        ref.getDocument { (document, error) in
+            if let document = document, document.exists {
+                //データの更新
+                ref.updateData([
+                    //配列データに追加
+                    Constants.FStore.titleField: FieldValue.arrayUnion(title),
+                    Constants.FStore.profField: FieldValue.arrayUnion(profileImageURL),
+                    Constants.FStore.bodyField: FieldValue.arrayUnion(body),
+                    Constants.FStore.tagsField: FieldValue.arrayUnion(tags),
+                    Constants.FStore.urlField: FieldValue.arrayUnion(url)
+                ]) { (error) in
+                    if error != nil {
+                        print("don't update data \(error)")
+                    }
+                }
+            } else {
                 //初期データの追加
-                bookmarkRef.setData([
+                ref.setData([
                     Constants.FStore.titleField: title,
                     Constants.FStore.profField: profileImageURL,
                     Constants.FStore.bodyField: body,
                     Constants.FStore.tagsField: tags,
                     Constants.FStore.urlField: url
-                ], merge: true)
-                return
+                ], merge: true) { (error) in
+                    if error != nil {
+                        print("don't set data \(error)")
+                    }
+                }
             }
         }
-        print("success update DB")
     }
   
     // MARK: - Access DB to fetch articles data
